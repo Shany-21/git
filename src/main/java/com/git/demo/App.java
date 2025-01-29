86b5eee0e88c7aae309a2b49974a82c401c9c391
unknown <shanysweetlin@gmail.com>
2025-01-29 17:33:33 +0530

    added

--- src/main/java/com/git/demo/App.java
+++ src/main/java/com/git/demo/App.java
@@ -1,13 +1,16 @@
 package com.git.demo;
-
-/**
- * Hello world!
- *
- */
+ 
 public class App 
 {
+	public void addData(String data) {
+		//System.out.println("data=" +data);
+		if(data.equals("Cons.DATA_TYPE")) {
+			System.out.prinltn("Welcome");
+		}
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+       
     }
 }
